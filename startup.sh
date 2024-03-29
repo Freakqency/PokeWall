7177e08e0710915915e449e9d77fda6974f4db02
Freakqency <suryashankar649@gmail.com>
2024-03-29 17:42:59 -0400

    added startup.sh file

---
+++ startup.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo "passing env from outer container to inner container"
+eval $(printenv | sed -n "s/^\([^=]\+\)=\(.*\)$/export \1=\2/p" | sed 's/"/\\\"/g' | sed '/=/s//="/' | sed 's/$/"/' >> /etc/profile)
