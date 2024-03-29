2d2ed8fc89b815218fcc0da26cae7c964149ac27
Freakqency <suryashankar649@gmail.com>
2024-03-29 18:53:23 -0400

    Updated startup script

--- startup.sh
+++ startup.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 echo "passing env from outer container to inner container"
-eval $(printenv | sed -n "s/^\([^=]\+\)=\(.*\)$/export \1=\2/p" | sed 's/"/\\\"/g' | sed '/=/s//="/' | sed 's/$/"/' >> /etc/profile)
+# Get environment variables to show up in SSH session
+eval $(printenv | awk -F= '{print "export " "\""$1"\"""=""\""$2"\"" }' >> /etc/profile)
 uvicorn main:app --host 0.0.0.0 --port 8000
